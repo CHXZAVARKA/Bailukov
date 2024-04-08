073f428059722e4765fa742abad4748aa984a7b0
CHXZAVARKA <94059106+CHXZAVARKA@users.noreply.github.com>
2024-04-08 11:15:27 +0300

    Add files via upload

--- TEST.py
+++ TEST.py
@@ -1,8 +1,13 @@
-a="hello"
-a=[1,1.0,"hello"]
-a=(1,1.0,'hello')
-a={1,1.0,"hello"}
-a=frozenset({1,1.0,"hello"})
-a={"a":1,"b":2}
-a=b"hello"
-a=bytearray(b"hello")
+a = "hello"
+a = [1,1.0, "hello"]
+a = (1,1.0, "hello")
+a = {1,1.0, "hello"}
+a = frozenset({1, 1.0, "hello"})
+a = {"a":1, "b":2}
+a = b"hello"
+a = bytearray(b"hello")
+a = [1, 1.0, "hello"]help(a)
+print(a)print(a)
+b =input("Введите число")len(a)
+min(a)max(a)
+type(a)
